5c3d10d8ebfdcdec831235863d81fa5d8c00fc93
bigzeze <965511871@qq.com>
2023-03-27 21:04:37 +0800

    tracing and sysinfo

--- kernel/proc.c
+++ kernel/proc.c
@@ -312,6 +312,9 @@ fork(void)
 
   pid = np->pid;
 
+  //mask carry
+  np->mask = p->mask;
+
   release(&np->lock);
 
   acquire(&wait_lock);
@@ -680,4 +683,28 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+// Return the number of processes whose state is not UNUSED
+uint64
+nproc(void)
+{
+  struct proc *p;
+  // counting the number of processes
+  uint64 num = 0;
+  // traverse all processes
+  for (p = proc; p < &proc[NPROC]; p++)
+  {
+    // add lock
+    acquire(&p->lock);
+    // if the processes's state is not UNUSED
+    if (p->state != UNUSED)
+    {
+      // the num add one
+      num++;
+    }
+    // release lock
+    release(&p->lock);
+  }
+  return num;
 }
